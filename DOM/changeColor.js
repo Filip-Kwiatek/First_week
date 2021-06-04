ea83121bf619c20171450d3a12fc774ecbd35b99
Filip-Kwiatek <84708010+Filip-Kwiatek@users.noreply.github.com>
2021-06-04 19:07:27 +0200

    Add files via upload

--- DOM/changeColor.js
+++ DOM/changeColor.js
@@ -1,13 +1,10 @@
-function Color() {
-  if ((document.getElementById("blue").style.color = "blue")) {
-    document.getElementById("square").style.backgroundColor = "blue";
-  } else if ((document.getElementById("red").style.color = "red")) {
-    document.getElementById("square").style.backgroundColor = "red";
-  } else if ((document.getElementById("yellow").style.color = "yellow")) {
-    document.getElementById("square").style.backgroundColor = "yellow";
-  } else if ((document.getElementById("green").style.color = "green")) {
-    document.getElementById("square").style.backgroundColor = "green";
-  } else {
-    document.getElementById("square").style.color = "black";
-  }
+function color(colorName) {
+  document.getElementById("square").style.backgroundColor = colorName;
 }
+
+/* window.onload = function() {
+  document.getElementById("button-yellow").addEventListener("mouseover", function() {
+    color('yellow')
+  });
+}
+*/
---
+++ DOM/firstPage.js
@@ -0,0 +1,9 @@
+function change(){
+    console.log("My first dynamic page");
+}
+function firstTitle() {
+    alert("The Beatles (January 13, 1969)");
+}
+function secondTitle() {
+    alert("Carly Rae Jepsen (March 1, 2012)");
+}
